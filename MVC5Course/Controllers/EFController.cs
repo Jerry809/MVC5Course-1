c1324f047438e4c4d1c972322d9c016886f749f6
Jerry <Jerry@LAPTOP-FHROAQ3A>
2016-10-16 15:31:32 +0800

    24 練習匯入資料庫中的 vw_ClientContribution 檢視表，並顯示資料在畫面上

--- MVC5Course/Controllers/EFController.cs
+++ MVC5Course/Controllers/EFController.cs
@@ -82,5 +82,11 @@ namespace MVC5Course.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult ClientContribution()
+        {
+            var data = db.vw_ClientContribution.Take(20);
+            return View(data);
+        }
     }
 }
